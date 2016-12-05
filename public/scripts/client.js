113ffa2834e62be11691f0b058fec94d5ab30df6
Andrew Froiland <afroiland@Andrews-MacBook-Pro-2.local>
2016-12-05 15:46:01 -0600

    nav working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,29 @@
 var app = angular.module('myApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl: '/views/templates/home.html',
+      controller: 'HomeController',
+      controllerAs: 'home'
+    })
+    .when('/profile' ,{
+      templateUrl: '/views/templates/profile.html',
+      controller: 'ProfileController',
+      controllerAs: 'profile'
+    })
+    .when('/leaderboard' ,{
+      templateUrl: '/views/templates/leaderboard.html',
+      controller: 'LeaderboardController',
+      controllerAs: 'leaderboard'
+    })
+    .when('/about' ,{
+      templateUrl: '/views/templates/about.html',
+      controller: 'AboutController',
+      controllerAs: 'about'
+    })
+    .otherwise({
+      redirectTo: 'home'
+    });
+
+}]);
--- server/app.js
+++ server/app.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var app = express();
-var path = require("path");
+var path = require('path');
+var profile = require('./routes/profile');
+var leaderboard = require('./routes/leaderboard');
+var about = require('./routes/about');
 var port = 3000;
 
 app.set("port", (process.env.PORT || port));
+
+app.use('/profile', profile);
+
+app.use('/leaderboard', leaderboard);
+
+app.use('/about', about);
+
+app.use(express.static(path.resolve('./public')));
+
+app.get('/home', function(req, res) {
+    res.send("hello from the server");
+});
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../public/views/index.html'));
