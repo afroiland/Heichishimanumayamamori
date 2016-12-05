2283f63457e4b1a0f33d5f63eb15c95288c81a96
Andrew Froiland <afroiland@Andrews-MacBook-Pro-2.local>
2016-12-04 19:42:32 -0600

    talking to server

---
+++ server/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+var port = 5000;
+
+app.set("port", (process.env.PORT || port));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/views/index.html'));
+});
+
+app.listen(app.get("port"), function(){
+    console.log("Server up and running on Port", port);
+});
