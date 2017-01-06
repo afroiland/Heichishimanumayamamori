56f69744cb3a55bd9576e19ad1eb29bbd1d7fb05
Andrew Froiland <andrew.froiland@gmail.com>
2017-01-05 20:02:16 -0600

    modifying database-config.js for Heroku

--- server/modules/database-config.js
+++ server/modules/database-config.js
@@ -1,1 +1,15 @@
-module.exports = 'postgres://localhost:5432/sigma';
+
+var connectionString = undefined;
+
+// If we are running on Heroku, use the remote database (with SSL)
+if(process.env.DATABASE_URL != undefined) {
+    connectionString = process.env.DATABASE_URL + "?ssl=true";
+} else {
+    // running locally, use our local database instead
+    connectionString = 'postgres://localhost:5432/sigma';
+}
+
+
+
+
+module.exports = connectionString;
